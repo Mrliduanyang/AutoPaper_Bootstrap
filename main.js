d0f8685fefc8ac20a7cdb6459c33848e681e8306
Mrliduanyang <liduanyangchina@gmail.com>
2018-03-11 23:56:26 +0800

    提交

---
+++ main.js
@@ -0,0 +1,31 @@
+const electron = require('electron');
+const { app } = electron;
+const { BrowserWindow } = electron;
+let win;
+function createWindow() {
+    // 创建窗口并加载页面
+    win = new BrowserWindow({width: 1200, height: 700});
+    win.loadURL(`file://${__dirname}/app/index.html`);
+    win.setMenuBarVisibility(false);
+
+
+    // 打开窗口的调试工具
+    // win.webContents.openDevTools();
+    // 窗口关闭的监听
+    win.on('closed', () => {
+        win = null;
+    });
+}
+
+app.on('ready', createWindow);
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (win === null) {
+        createWindow();
+    }
+});
